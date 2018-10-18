915c2b867c8f21806fc4759b9b5ac03ca57c2af9
Yang Yang <yyang@gnshealthcare.com>
2018-10-18 03:04:14 +0000

    update several functions

--- snippet_class_desig.r
+++ snippet_class_desig.r
@@ -3,7 +3,7 @@ library(purrr)
 library(dplyr)
 
 
-#: a function
+#: -----a function--------------
 class_designation <- function(dt, varNames) {
   # @desc: ths logic is we have all character type columns in the beginning (regardless of column values), 
   # if can coerce to numeric value, do it; otherwise(try and error/warning), coerce to factor. If error again, will stop,
@@ -19,9 +19,16 @@ class_designation <- function(dt, varNames) {
 
 }
 # then manually assign the dt's varNames columns with returned new values (after type conversion)                            
+##:----example code below----
+##: execute `class_designation`
+#replace_dt_part <- class_designation(joined_dt, varNames)
+#
+#joined_dt[ , (varNames) := replace_dt_part]
 
 
-#: a function                              
+
+
+#: -----a function--------------
 continuous_binnedCategorical <- function(dt, varNames, n_levels = 10, 
                                          cut_func = c(ggplot2::cut_number, ggplot2::cut_interval)) {
   # @desc: ths logic is at first check if those cols of varNames are numeric and integer columns,
@@ -55,6 +62,38 @@ continuous_binnedCategorical <- function(dt, varNames, n_levels = 10,
 
 
 }                              
-
-
-
+# then manually assign the dt's varNames columns with returned new values (after type conversion)                            
+###:----example code below----
+##: execute `class_designation`
+#varNames_continuousVars <- colClass_dt[ class == "numeric", varName ]
+#
+#foreach (n_l = seq(10,100,10)) %do% {
+#      
+#      #: 1st, use `ggplot2::cut_number`
+#      replace_dt_part <- continuous_binnedCategorical(joined_dt, varNames_continuousVars, n_levels = n_l, cut_func = ggplot2::cut_number )
+#    
+#    #: create new columns in the dt
+#    joined_dt[ , (sprintf( "equalSizeBinned_max%dlevels_%s", n_l, varNames_continuousVars)) := replace_dt_part]
+#
+#        
+#        #: 2nd, use `ggplot2::cut_interval` .
+#        replace_dt_part <- continuous_binnedCategorical(joined_dt, varNames_continuousVars, n_levels = n_l, cut_func = ggplot2::cut_interval )
+#        
+#        #: create new columns in the dt
+#        joined_dt[ , (sprintf( "equalRangeBinned_max%dlevels_%s", n_l, varNames_continuousVars)) := replace_dt_part]
+#
+#            #: since above replace in place, so here dont return anything.
+#            return(NULL)
+#            
+#}
+##: after `class_designation`, joined_dt may actually have same content columns (with different colnames though), libray 'digest' could produce hash tag on R object, same hash tag represents same contents columns, so can be removed to save memory (i.e., reduce the size of dt object). 
+#library(digest)
+#
+#dup_col_index <- duplicated(map_chr(joined_dt, digest))
+#
+#dup_col_index %>% mean
+#
+#joined_dt <- joined_dt[ , !dup_col_index, with = FALSE]
+#
+#catt("Expanded joined_dt dim is:", dim(joined_dt))
+#
